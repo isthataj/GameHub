bbbe3d1817489259f482676320ac5b91b5ea660f
ericshark <eric122409@gmail.com>
2024-05-07 13:04:35 -0400

    pp

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -5,9 +5,9 @@ public class SnakeGame {
     public static void main(String[] args){
 
         new GameFrame();
-        //hello world!
 
-        //hello p
+        TicTacToe TicTac = new TicTacToe();
+
     }
 
 
---
+++ src/TicTacToe.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JPanel;
+import javax.swing.*;
+import java.util.Random;
+
+
+public class TicTacToe {
+}
