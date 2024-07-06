35f65bc069a445e1c2535cf6d48ad41c64e00562
isthataj <99852904+isthataj@users.noreply.github.com>
2024-07-06 00:20:11 -0400

    updated comments

--- javadocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"GameFrame"},{"p":"<Unnamed>","l":"MainMenu"},{"p":"<Unnamed>","l":"MiniTic"},{"p":"<Unnamed>","l":"SnakeGame.MyKeyAdapter"},{"p":"<Unnamed>","l":"SnakeGame"},{"p":"<Unnamed>","l":"TicTacToe"}];updateSearchResults();
